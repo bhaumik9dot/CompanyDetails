a80b74fe34038816a431a4a00a1fb24f0e33e8bc
Bhaumik Vachhani <bhaumik.9dot@gmail.com>
2025-01-23 17:25:07 +0530

    add-migration

---
+++ Migrations/20250123115312_M5.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompanyDetails.Migrations
+{
+    /// <inheritdoc />
+    public partial class M5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "iOSBuild",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WhatsNew",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Suspended",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PlatformFeatures",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MacAddress",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IpAddressV4",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ForceUpgrade",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DemoAccountNodeId",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Canceled",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApiBaseUrl",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AndroidBuild",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AcquisitionDate",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountsCreatorURL",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.UpdateData(
+                table: "RoleMaster",
+                keyColumn: "RoleId",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 1, 23, 11, 53, 12, 440, DateTimeKind.Utc).AddTicks(2100));
+
+            migrationBuilder.UpdateData(
+                table: "RoleMaster",
+                keyColumn: "RoleId",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 1, 23, 11, 53, 12, 440, DateTimeKind.Utc).AddTicks(2149));
+
+            migrationBuilder.UpdateData(
+                table: "UserMaster",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 1, 23, 11, 53, 12, 440, DateTimeKind.Utc).AddTicks(2023));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "iOSBuild",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WhatsNew",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Suspended",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PlatformFeatures",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MacAddress",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IpAddressV4",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ForceUpgrade",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DemoAccountNodeId",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Canceled",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApiBaseUrl",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AndroidBuild",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AcquisitionDate",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountsCreatorURL",
+                table: "CompanyInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "RoleMaster",
+                keyColumn: "RoleId",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 1, 23, 8, 4, 8, 361, DateTimeKind.Utc).AddTicks(7150));
+
+            migrationBuilder.UpdateData(
+                table: "RoleMaster",
+                keyColumn: "RoleId",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 1, 23, 8, 4, 8, 361, DateTimeKind.Utc).AddTicks(7202));
+
+            migrationBuilder.UpdateData(
+                table: "UserMaster",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 1, 23, 8, 4, 8, 361, DateTimeKind.Utc).AddTicks(7060));
+        }
+    }
+}
